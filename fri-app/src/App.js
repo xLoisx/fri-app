50b02c9b4d2413e041fe0385efcf46a51b53c937
John Lois Santos <113756930+xLoisx@users.noreply.github.com>
2022-12-03 11:13:33 +0800

    2nd commit

--- fri-app/src/App.js
+++ fri-app/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Navbar/>
       <div>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/fri-app/" element={<Home/>}/>
           <Route path="/Home" element={<Home/>}/>
           <Route path="/Pricing" element={<Pricing/>}/>
           <Route path="/Features" element={<Features/>}/>
